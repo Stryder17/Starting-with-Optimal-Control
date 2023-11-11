39e459849d1e2cadd682b2a8d41dd3374741b441
Manjul Regmi <manjulregmi@gmail.com>
2023-11-10 23:39:26 -0600

    Update code

---
+++ Trajectory Optimization/Optimal_Pendulum_SwingUp.m
@@ -0,0 +1,22 @@
+% Introduction to Trajectory Optimization by Matthew Kelly
+
+clc;clear;close all;
+
+% System Dynamics
+
+
+% Initial Condition
+
+
+% Final Condition (Boundary constraints)
+
+
+% Performance index
+% We want to minimize control and final state is fixed.
+
+
+% State and control limits
+
+
+% Optimal control
+
